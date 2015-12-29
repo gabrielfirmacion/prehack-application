55b382a6b562c9098b83ebb64a0fdf369ef431bd
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2015-12-29 15:48:07 -0800

    do form for part 2 and create a thank you page to be rendered

--- prehack-application/app/controllers/part1_controller.rb
+++ prehack-application/app/controllers/part1_controller.rb
@@ -6,12 +6,16 @@ class Part1Controller < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    p user_params
+    @user.blog_title1 = user_params[:blog_title1]
+    @user.blog_explanation1 = user_params[:blog_explanation1]
+    @user.save!
 
     p "**********"
     p @user.first_name
     p "**********"
     p @user.blog_title1
+    p "**********"
+    p @user.blog_explanation1
     redirect_to user_part2_index_path(@user.id)
 
   end
--- prehack-application/app/controllers/part2_controller.rb
+++ prehack-application/app/controllers/part2_controller.rb
@@ -5,5 +5,23 @@ class Part2Controller < ApplicationController
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @user.blog_title2 = user_params[:blog_title2]
+    @user.blog_explanation2 = user_params[:blog_explanation2]
+    @user.save!
+
+    p "**********"
+    p @user.first_name
+    p "**********"
+    p @user.blog_title2
+    p "**********"
+    p @user.blog_explanation2
+
+    render "thankyou"
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:blog_title2, :blog_explanation2)
+    end
 end
